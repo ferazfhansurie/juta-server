3e60999254f3d7ead5e7595ab9f95331f300f781
shafiqps <shafiqaimanps@gmail.com>
2024-08-15 14:18:09 +0800

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,12 @@ module.exports = {
   apps: [{
     name: "server",
     script: "server.js",
-    instances: "max",
-    exec_mode: "cluster",
     watch: true,
     wait_ready: true,
     listen_timeout: 10000,
     kill_timeout: 5000,
     env: {
+      PORT: 8443,
       NODE_ENV: "production",
     }
   }]
